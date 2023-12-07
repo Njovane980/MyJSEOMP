4a23e280158c41de9edac7b884622a9e11dd9d4c
Njovane980 <vuyisekanjovane8@gmail.com>
2023-12-07 14:09:27 +0200

    Committing changes before switching to main

--- script/checkout.js
+++ script/checkout.js
@@ -3,19 +3,21 @@ let checkOut = JSON.parse(localStorage.getItem("cart")) || [];
 function displayCheckoutItems() {
   let checkoutContainer = document.getElementById("checkoutContainer");
   checkoutContainer.innerHTML = "";
-    let data = Object.groupBy(checkOut, item=> item.name)
-    // console.log(data);
-    for(let key in data) {
-        // console.log(key, data[key]);
-        checkoutContainer.innerHTML +=`
+  let data = Object.groupBy(checkOut, (item) => item.name);
+  // console.log(data);
+  for (let key in data) {
+    // console.log(key, data[key]);
+    checkoutContainer.innerHTML += `
             <tr>
                 <td>${data[key][0].name}</td>
                 <td>${data[key].length}</td>
                 <td>R${data[key][0].price}</td>
-                <td>R${eval(`${data[key][0].price} * ${data[key].length}`)}  </td>
+                <td>R${eval(
+                  `${data[key][0].price} * ${data[key].length}`
+                )}  </td>
             </tr>
-        `
-    }
+        `;
+  }
 }
 //Initial display of checkout items
 displayCheckoutItems();
